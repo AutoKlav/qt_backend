bd224c8926217f759fd1850ec57984b948d20847
Spike433 <juhacar116@gmail.com>
2024-11-24 16:17:03 +0100

    Refactor process log creation in StateMachine::calculateDrFrRValuesFromSensors()

--- statemachine.cpp
+++ statemachine.cpp
@@ -105,10 +105,7 @@ StateMachineValues StateMachine::calculateDrFrRValuesFromSensors(int processId)
         stateMachineValues.sumr = 0;
     }
 
-    // ProcessId will be only -1 when it is requested on the fly, from Grpc server
-    if(processId!=-1){
-        DbManager::instance().createProcessLog(processId);
-    }
+    DbManager::instance().createProcessLog(processId);
 
     return stateMachineValues;
 }
